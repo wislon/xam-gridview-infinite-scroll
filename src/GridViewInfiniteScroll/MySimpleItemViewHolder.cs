44ed929d6dc9fff78910373e2e2877af55e7e504
John W <wislon@gmail.com>
2015-02-21 17:02:00 +1000

    add MySimpleItemViewHolder

---
+++ src/GridViewInfiniteScroll/MySimpleItemViewHolder.cs
@@ -0,0 +1,14 @@
+﻿using Android.Widget;
+
+namespace GridViewInfiniteScroll
+{
+  /// <summary>
+  /// Acts as a 'pointer' to the controls in the view, so we 
+  /// only need to find them the very first time we inflate it
+  /// </summary>
+  public class MySimpleItemViewHolder : Java.Lang.Object
+  {
+    public TextView DisplayName { get; set; }
+    public ImageView Thumbnail { get; set; }
+  }
+}
