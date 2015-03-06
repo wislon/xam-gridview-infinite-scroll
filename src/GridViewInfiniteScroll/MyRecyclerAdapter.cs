e3fb6dee3b69f90d79f9d31fcd157eb415b64bee
John W <wislon@gmail.com>
2015-03-06 16:05:28 +1000

    add MyRecyclerAdapter stub

---
+++ src/GridViewInfiniteScroll/MyRecyclerAdapter.cs
@@ -0,0 +1,24 @@
+﻿using Android.Support.V7.Widget;
+using Android.Views;
+
+
+namespace GridViewInfiniteScroll
+{
+  public class MyRecyclerAdapter : RecyclerView.Adapter
+  {
+    public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    public override int ItemCount
+    {
+      get { throw new System.NotImplementedException(); }
+    }
+  }
+}
