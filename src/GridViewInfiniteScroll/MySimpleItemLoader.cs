4d31caae8b6d8ef8096322488cab9717a05acdb4
John W <wislon@gmail.com>
2015-03-08 14:38:25 +1000

    add some simple lorem ipsum random stuff to randomise descriptions and cause staggering.

--- src/GridViewInfiniteScroll/MySimpleItemLoader.cs
+++ src/GridViewInfiniteScroll/MySimpleItemLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Text;
+using Java.Util;
 
 namespace GridViewInfiniteScroll
 {
@@ -10,17 +11,23 @@ namespace GridViewInfiniteScroll
     public bool IsBusy { get; set; }
     public int CurrentPageValue { get; set; }
 
+    private readonly string[] _lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.".Split(' ');
+    private readonly Random _rand;
+
     public MySimpleItemLoader()
     {
       MySimpleItems = new List<MySimpleItem>();
+      _rand = new Random();
     }
+
 
     public void LoadMoreItems(int itemsPerPage)
     {
       IsBusy = true;
       for (int i = CurrentPageValue; i < CurrentPageValue + itemsPerPage; i++)
       {
-        MySimpleItems.Add(new MySimpleItem(){DisplayName = string.Format("This is item {0:0000}", i)});
+        string randomLorem = string.Join(" ", _lorem, 0, _rand.NextInt(_lorem.Length - 1));
+        MySimpleItems.Add(new MySimpleItem(){DisplayName = string.Format("This is item {0:0000} ({1})", i, randomLorem)});
       }
       // normally you'd check to see if the number of items returned is less than the number requested, i.e. you've run out, and then set this accordinly.
       CanLoadMoreItems = true;
